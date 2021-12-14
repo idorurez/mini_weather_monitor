b44d37d28f34740c094bf22e4b84b930881b9a52
alfred <alfredy@gmail.com>
2021-12-13 22:32:24 -0800

    futzing with text sizing

--- lib/tft_setup.h
+++ lib/tft_setup.h
@@ -9,30 +9,25 @@
 //   Note that some sketches are designed for a particular TFT pixel width/height
 
 // Only define one driver, the other ones must be commented out
-#define ILI9341_DRIVER
-#define ESP32_PARALLEL
-// The ESP32 and TFT the pins used for testing are:
+
+
+#define ILI9486_DRIVER
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
 #define TFT_CS   33  // Chip select control pin (library pulls permanently low
 #define TFT_DC   22  // (RS) Data Command control pin - must use a pin in the range 0-31
 #define TFT_RST  32  // Reset pin, toggles on startup
-#define TFT_WR    21  // Write strobe control pin - must use a pin in the range 0-31
-#define TFT_RD   17  //15  // Read strobe control pin
-#define TFT_D0   2  // Must use pins in the range 0-31 for the data bus
-#define TFT_D1   13  // so a single register write sets/clears all bits.
-#define TFT_D2   26  // Pins can be randomly assigned, this does not affect
-#define TFT_D3   25  // TFT screen update performance.
-#define TFT_D4   27
-#define TFT_D5   12
-#define TFT_D6   14
-#define TFT_D7   4
+#define TFT_BL   4  // LED back-light
+#define SD_PIN 5
 
-#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
-#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
-#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
-#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
-#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+// #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+// #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+// #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+// #define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+// #define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
 #define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+// #define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
 
 #define SMOOTH_FONT
 #define SPI_FREQUENCY  27000000
