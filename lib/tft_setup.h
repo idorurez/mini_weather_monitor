bf18b43d0f8e6c9818b71c2c970099c89e22236c
alfred <alfredy@gmail.com>
2022-01-23 18:01:19 -0800

    updated pcb and everything is working so far

--- lib/tft_setup.h
+++ lib/tft_setup.h
@@ -12,22 +12,34 @@
 
 
 #define ILI9486_DRIVER
-#define TFT_MISO 19
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS   33  // Chip select control pin (library pulls permanently low
-// #define TFT_DC   22  // (RS) Data Command control pin - must use a pin in the range 0-31
+
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15  // Chip select control pin (library pulls permanently low
+
 #define TFT_DC   26  // (RS) Data Command control pin - must use a pin in the range 0-31
 #define TFT_RST  32  // Reset pin, toggles on startup
 #define TFT_BL   4  // LED back-light
-#define SD_PIN 5
+
+#define SD_PIN      5
+#define SD_MISO     19
+#define SD_MOSI     23
+#define SD_SCLK     18
+
+#define PWM1_CH    0
+#define PWM1_RES   8
+#define PWM1_FREQ 70588
+
+#define TFT_W 480
+#define TFT_H 320
 
 // #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 // #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
 // #define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
 // #define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
 // #define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+// #define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
 // #define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
 
 #define SMOOTH_FONT
