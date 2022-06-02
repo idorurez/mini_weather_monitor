bd3601420224b93ffcbdd2bf56da199a635f2e01
alfred <alfredy@gmail.com>
2022-06-01 23:08:19 -0700

    fixxing footprint and pad

---
+++ .pio/build/esp32dev/libfaf/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/libfaf/SPI/SPI.cpp.o: \
+ C:/Users/neuro/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/neuro/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/neuro/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/neuro/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
